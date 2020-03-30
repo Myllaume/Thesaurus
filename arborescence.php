40c6e2064e247fafcbf64c50c0c35b301b39f9a1
Guillaume Brioudes <guillaume.brioudes@myllaume.fr>
2020-03-30 15:45:47 +0200

    génération d'un cache CSV ou JSON

--- arborescence.php
+++ arborescence.php
@@ -29,6 +29,23 @@ function search_ascendant($bdd, $id) {
     return $concepts_list;
 }
 
+function create_JSON_from_array($array, $path) {
+    $JSON_content = json_encode($array, JSON_UNESCAPED_UNICODE);
+    file_put_contents($path, $JSON_content);
+}
+
+function create_CSV_from_array($array, $path, $entete) {
+    $CSV_file = fopen($path, 'w');
+
+    fputcsv($CSV_file, $entete);
+
+    foreach ($array as $line) {
+        fputcsv($CSV_file, $line);
+    }
+
+    fclose($CSV_file);
+}
+
 /**
  * -------
  * Génration de l'arborescence
@@ -48,17 +65,36 @@ function gen_arborescence($bdd, $concepts_list) {
     echo '</ul>';
 }
 
-function gen_cache($bdd, $concepts_list) {
-    if (!$concepts_list) { return; }
-    echo '<ul>';
+gen_arborescence($bdd, search_ascendant($bdd, 0));
 
-    foreach ($concepts_list as $nb => $value) {
-        echo '<li>' . $value['nom'] . '</li>';
+gen_cache($bdd);
 
-        gen_arborescence($bdd, search_ascendant($bdd, $value['id']));
+function gen_cache($bdd) {
+    $array = [];
+
+    $request = $bdd->prepare('SELECT * FROM Concepts');
+    $is_valid_request = $request->execute();
+
+    if (!$is_valid_request) {
+        // Erreur bdd : SELECT Concepts
+        return false;
     }
 
-    echo '</ul>';
+    $concepts_list = $request->fetchAll(PDO::FETCH_ASSOC);
+    if (empty($concepts_list)) {
+        // Aucun concept trouvé dans la base de données
+        return false;
+    }
+
+    foreach ($concepts_list as $nb => $value) {
+        array_push($array, [
+            'id' => $value['id'],
+            'nom' => $value['nom'],
+            'id_ascendant' => $value['id_ascendant']
+        ]);
+    }
+
+    create_CSV_from_array($array, './cache.csv', ['id', 'nom', 'id_ascendant']);
+    // create_JSON_from_array($array, './cache.json');
 }
 
-gen_arborescence($bdd, search_ascendant($bdd, 0));
